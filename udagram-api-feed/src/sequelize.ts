643fd69590b7dda8159edbaeadaeb7eaa4e439b3
Omair Muhi <omairmuhi@icloud.com>
2022-01-25 10:16:41 -0500

    Subject: Add debug logs Problem: ECONNREFUSED 127.0.0.1:5432 Solution Note: Seen in k8s pod environment when connecting to psql db

--- udagram-api-feed/src/sequelize.ts
+++ udagram-api-feed/src/sequelize.ts
@@ -1,13 +1,16 @@
-import {Sequelize} from 'sequelize-typescript';
-import {config} from './config/config';
+import { Sequelize } from "sequelize-typescript";
+import { config } from "./config/config";
 
-
+console.log(`config.username=${config.username}`);
+console.log(`config.password=${config.password}`);
+console.log(`config.database=${config.database}`);
+console.log(`config.host=${config.host}`);
 export const sequelize = new Sequelize({
-  'username': config.username,
-  'password': config.password,
-  'database': config.database,
-  'host': config.host,
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
 
-  'dialect': config.dialect,
-  'storage': ':memory:',
+  dialect: config.dialect,
+  storage: ":memory:",
 });
