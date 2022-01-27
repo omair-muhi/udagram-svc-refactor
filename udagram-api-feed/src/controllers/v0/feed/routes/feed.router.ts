79af70ea40ba699d4f8bfa48586f51888d4a271c
Omair Muhi <omairmuhi@icloud.com>
2022-01-27 11:50:21 -0500

    Subject: Update logic for extracting JWT token Problem Solution Note

--- udagram-api-feed/src/controllers/v0/feed/routes/feed.router.ts
+++ udagram-api-feed/src/controllers/v0/feed/routes/feed.router.ts
@@ -1,34 +1,58 @@
-import {Router, Request, Response} from 'express';
-import {FeedItem} from '../models/FeedItem';
-import {NextFunction} from 'connect';
-import * as jwt from 'jsonwebtoken';
-import * as AWS from '../../../../aws';
-import * as c from '../../../../config/config';
+import { Router, Request, Response } from "express";
+import { FeedItem } from "../models/FeedItem";
+import { NextFunction } from "connect";
+import * as jwt from "jsonwebtoken";
+import * as AWS from "../../../../aws";
+import * as c from "../../../../config/config";
 
 const router: Router = Router();
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
+  console.log("Checking for authorization headers");
   if (!req.headers || !req.headers.authorization) {
-    return res.status(401).send({message: 'No authorization headers.'});
+    return res.status(401).send({ message: "No authorization headers." });
   }
+  console.log("Authorization headers OK");
+  console.log("----REQ body-----");
+  console.log(req.body);
+  console.log("-----REQ body---");
+  console.log("----REQ headers-----");
+  console.log(req.headers);
+  console.log("-----REQ headers---");
+  console.log("----REQ authorization-----");
+  console.log(req.headers.authorization);
+  console.log("-----REQ authorization---");
 
-  const tokenBearer = req.headers.authorization.split(' ');
-  if (tokenBearer.length != 2) {
-    return res.status(401).send({message: 'Malformed token.'});
+  console.log("Checking for malformed token");
+  console.log("---BEFORE length", req.headers.authorization.length);
+  const tokenBearerTest = req.headers.authorization.split(",");
+  const tokenBearerTest2 = tokenBearerTest[tokenBearerTest.length - 1];
+  console.log(`tokenBearerTest2 --- ${tokenBearerTest2}`);
+  const tokenBearerTest3 = tokenBearerTest2.split(" ");
+  console.log(`tokenBearerTest3 --- ${tokenBearerTest3}`);
+  // const tokenBearer = req.headers.authorization.split(" ");
+  if (tokenBearerTest3.length != 2) {
+    return res.status(401).send({ message: "Malformed token." });
+  } else {
+    console.log("tokenBearerTest3 is OK");
   }
-
-  const token = tokenBearer[1];
+  console.log("Checking JWT");
+  const token = tokenBearerTest3[1];
   return jwt.verify(token, c.config.jwt.secret, (err, decoded) => {
     if (err) {
-      return res.status(500).send({auth: false, message: 'Failed to authenticate.'});
+      console.log("Failed to authenticate");
+      return res
+        .status(500)
+        .send({ auth: false, message: "Failed to authenticate." });
     }
+    console.log("Authentication succeeded");
     return next();
   });
 }
 
 // Get all feed items
-router.get('/', async (req: Request, res: Response) => {
-  const items = await FeedItem.findAndCountAll({order: [['id', 'DESC']]});
+router.get("/", async (req: Request, res: Response) => {
+  const items = await FeedItem.findAndCountAll({ order: [["id", "DESC"]] });
   items.rows.map((item) => {
     if (item.url) {
       item.url = AWS.getGetSignedUrl(item.url);
@@ -38,46 +62,47 @@ router.get('/', async (req: Request, res: Response) => {
 });
 
 // Get a feed resource
-router.get('/:id',
-    async (req: Request, res: Response) => {
-      const {id} = req.params;
-      const item = await FeedItem.findByPk(id);
-      res.send(item);
-    });
+router.get("/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const item = await FeedItem.findByPk(id);
+  res.send(item);
+});
 
 // Get a signed url to put a new item in the bucket
-router.get('/signed-url/:fileName',
-    requireAuth,
-    async (req: Request, res: Response) => {
-      const {fileName} = req.params;
-      const url = AWS.getPutSignedUrl(fileName);
-      res.status(201).send({url: url});
-    });
+router.get(
+  "/signed-url/:fileName",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { fileName } = req.params;
+    const url = AWS.getPutSignedUrl(fileName);
+    res.status(201).send({ url: url });
+  }
+);
 
 // Create feed with metadata
-router.post('/',
-    requireAuth,
-    async (req: Request, res: Response) => {
-      const caption = req.body.caption;
-      const fileName = req.body.url; // same as S3 key name
+router.post("/", requireAuth, async (req: Request, res: Response) => {
+  const caption = req.body.caption;
+  const fileName = req.body.url; // same as S3 key name
 
-      if (!caption) {
-        return res.status(400).send({message: 'Caption is required or malformed.'});
-      }
+  if (!caption) {
+    return res
+      .status(400)
+      .send({ message: "Caption is required or malformed." });
+  }
 
-      if (!fileName) {
-        return res.status(400).send({message: 'File url is required.'});
-      }
+  if (!fileName) {
+    return res.status(400).send({ message: "File url is required." });
+  }
 
-      const item = await new FeedItem({
-        caption: caption,
-        url: fileName,
-      });
+  const item = await new FeedItem({
+    caption: caption,
+    url: fileName,
+  });
 
-      const savedItem = await item.save();
+  const savedItem = await item.save();
 
-      savedItem.url = AWS.getGetSignedUrl(savedItem.url);
-      res.status(201).send(savedItem);
-    });
+  savedItem.url = AWS.getGetSignedUrl(savedItem.url);
+  res.status(201).send(savedItem);
+});
 
 export const FeedRouter: Router = router;
